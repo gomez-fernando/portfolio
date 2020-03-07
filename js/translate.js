73db65de75bc078d9755942683b6e79e6f6ff31c
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2020-03-07 19:31:17 +0100

    spanish traslation finished

--- js/translate.js
+++ js/translate.js
@@ -218,7 +218,219 @@ function english() {
 // spanish translation -------------------------
 // ---------------------------------------------
 function spanish() {
-  location.reload();
+
+  let welcome = document.getElementsByClassName("t_welcome");
+  for (let item of welcome) {
+    console.log(item.id);
+    item.innerHTML = "Bienvenido/as";
+  };
+
+  let portfolio = document.getElementsByClassName("t_portfolio");
+  for (let item of portfolio) {
+    console.log(item.id);
+    item.innerHTML = "Portafolio";
+  };
+
+  let home = document.getElementsByClassName("t_home");
+  for (let item of home) {
+    console.log(item.id);
+    item.innerHTML = "Inicio";
+  };
+
+  let contact = document.getElementsByClassName("t_contact");
+  for (let item of contact) {
+    console.log(item.id);
+    item.innerHTML = "Contacto";
+  };
+
+  let technologies = document.getElementsByClassName("t_technologies");
+  for (let item of technologies) {
+    console.log(item.id);
+    item.innerHTML = "Tecnologías";
+  };
+
+  let account = document.getElementsByClassName("t_account");
+  for (let item of account) {
+    console.log(item.id);
+    item.innerHTML = "En mi cuenta de ";
+  };
+
+  let find = document.getElementsByClassName("t_find");
+  for (let item of find) {
+    console.log(item.id);
+    item.innerHTML = "encontrarás el código de esta web.";
+  };
+
+  let visit = document.getElementsByClassName("t_visit");
+  for (let item of visit) {
+    console.log(item.id);
+    item.innerHTML = "Visitar el sitio web";
+  };
+
+  let course = document.getElementsByClassName("t_course");
+  for (let item of course) {
+    console.log(item.id);
+    item.innerHTML = "Curso realizado en la plataforma educativa ";
+  };
+
+  let certificates = document.getElementsByClassName("t_certificates");
+  for (let item of certificates) {
+    console.log(item.id);
+    item.innerHTML = "Certificados y Diplomas";
+  };
+
+  // Ids -------------------------------------
+  // -----------------------------------------
+  let t10 = document.getElementById("t10");
+  t10.innerHTML = "Fernando Gomez <br> Desarrollador Web";
+
+  let t40 = document.getElementById("t40");
+  t40.innerHTML = "En ésta página web podéis encontrar una selección de proyectos personales. También información académica sobre mí y datos de contacto.";
+
+  let t50 = document.getElementById("t50");
+  t50.innerHTML = "Actualmente me encuentro cursando el Ciclo Superior de FP en Desarrollo Web en la UOC (Universitat Oberta de Catalunya).";
+
+  let t60 = document.getElementById("t60");
+  t60.innerHTML = "También me desempeño como desarrollador web freelance para pequeñas empresas y particulares.";
+
+  let t70 = document.getElementById("t70");
+  t70.innerHTML = "Los lenguajes y frameworks con los que trabajo principalmente son: ";
+
+  let t80 = document.getElementById("t80");
+  t80.innerHTML = "¿Creamos algo juntos?";
+
+  let t90 = document.getElementById("t90");
+  t90.innerHTML = "Puedes contactarme a través de los siguientes links";
+
+  let t100 = document.getElementById("t100");
+  t100.innerHTML = "Sitio web para seguir la cotización de las principales criptomonedas. Utiliza la API de CoinCap para obtener todos los datos necesarios, además de mostrar una gráfica de la evolución del precio en las últimas 24 horas.";
+
+  let t110 = document.getElementById(
+    "t110"
+  );
+  t110.innerHTML =
+    "Tienda en línea desarrollada con Angular 8. Utilizando componentes, módulos y rutas. Conectada a la API REST de Platzi. Maquetada con Material Design y alojada en Firebase.";
+
+  let t120 = document.getElementById(
+    "t120"
+  );
+  t120.innerHTML = "Un CMS desarrollado íntegramente con Laravel 6. Utiliza una base de datos SQL. Se pueden gestionar completamente los usuarios, artículos de prensa y permisos de edición, publicación o eliminación de noticias. Desarrollado dentro del currículum académico de la UOC.";
+
+  let t130 = document.getElementById("t130");
+  t130.innerHTML =
+    "Una aplicación de control de tareas. Se puede abrir una cuenta y gestionar las tareas propias (CRUD: crear, consultar, editar y eliminar).";
+
+  let t140 = document.getElementById("t140");
+  t140.innerHTML =
+    "Este sitio utiliza XMLHttpRequest() para obtener datos aleatorios de usuarios y películas. Se utiliza Vanilla JS únicamente y forma parte del curso de jQuery a JavaScript de Platzi.";
+
+  let t150 = document.getElementById("t150");
+  t150.innerHTML = "Una red social desarrollada con Laravel 6. Los usuarios pueden comentar, dar likes y estrellas a los productos. Pueden hacer su lista de favoritos. El administrador puede bloquear usuarios y añadir productos y categorías. Desarrollado dentro del currículum académico de la UOC.";
+
+  let t160 = document.getElementById(
+    "t160"
+  );
+  t160.innerHTML =
+    "Diseño de un blog con resposive design utilizando jQuery. Cuenta con varias funcionalidades como un reloj, inicio de sesión, selector de tema, slider, botón de back-to-top y los posts son generados desde JS.";
+
+  let t170 = document.getElementById("t170");
+  t170.innerHTML =
+    "Responsive website del Instituto Nacional de Riesgos Laborales. Desarrollado dentro del currículum académico de la Universitat Oberta de Catalunya (UOC). Se pueden registrar partes de accidente, editarlos y consultarlos. Control de usuarios y sesiones. Utiliza una base de datos SQL.";
+
+  let t180 = document.getElementById(
+    "t180"
+  );
+  t180.innerHTML = "Layout de Instagram";
+
+  let t190 = document.getElementById(
+    "t190"
+  );
+  t190.innerHTML =
+    "Uso de CSS Grid para replicar el layout de la página de Instagram.";
+
+  let t200 = document.getElementById(
+    "t200"
+  );
+  t200.innerHTML =
+    "Simón dice con colores";
+
+  let t210 = document.getElementById("t210");
+  t210.innerHTML = "Juego de memorización donde hay que seguir la secuencia de colores iluminados para ir pasando de nivel. Se puede seleccionar el tema preferido entre 5 colores diferentes.";
+
+  let t220 = document.getElementById("t220");
+  t220.innerHTML =
+    "Tienda online Smart Wallet";
+
+  let t230 = document.getElementById("t230");
+  t230.innerHTML =
+    "Un sitio web responsive para vender monederos de criptomonedas. Se puede abrir una cuenta y hacer los pedidos desde el carrito de la compra. Utiliza una base de datos SQL y PHP con MVC. Desarrollado dentro del currículum académico de la UOC.";
+
+  let t240 = document.getElementById("t240");
+  t240.innerHTML = "Un sitio web multidioma para una start-up. Desarrollado con Laravel 6. Forma parte del currículum académico de la UOC.";
+
+  let t250 = document.getElementById("t250");
+  t250.innerHTML = "Curso de Symfony 5 (Master en PHP)";
+
+  let t260 = document.getElementById(
+    "t260"
+  );
+  t260.innerHTML =
+    "Curso de Angular 8";
+
+  let t270 = document.getElementById(
+    "t270"
+  );
+  t270.innerHTML =
+    "Curso de Vue.js";
+
+  let t280 = document.getElementById("t280");
+  t280.innerHTML = "Curso de Laravel 6";
+
+  let t290 = document.getElementById("t290");
+  t290.innerHTML =
+    "Curso de jQuery a JavaScript";
+
+  let t300 = document.getElementById("t300");
+  t300.innerHTML =
+    "Curso de Scrum";
+
+  let t310 = document.getElementById("t310");
+  t310.innerHTML = "Curso de EcmaScript 6+";
+
+  let t320 = document.getElementById("t320");
+  t320.innerHTML = "Curso Profesional de Git y Github";
+
+  let t330 = document.getElementById("t330");
+  t330.innerHTML =
+    "Curso de Bootstrap 4";
+
+  let t340 = document.getElementById("t340");
+  t340.innerHTML =
+    "Curso de CCS Grid";
+
+  let t350 = document.getElementById("t350");
+  t350.innerHTML = "Curso Introducción a PHP";
+
+  let t360 = document.getElementById("t360");
+  t360.innerHTML = "Curso Básico de JAVA SE";
+
+  let t370 = document.getElementById("t370");
+  t370.innerHTML =
+    "Curso de WordPress";
+
+  let t380 = document.getElementById("t380");
+  t380.innerHTML =
+    "Curso de Creación de temas y plugins para WordPress";
+
+  let t390 = document.getElementById("t390");
+  t390.innerHTML = "Curso Fundamentos de Javascript";
+
+  let t400 = document.getElementById("t400");
+  t400.innerHTML = "Curso Fundamentos de Bases de Datos";
+
+  let t410 = document.getElementById("t410");
+  t410.innerHTML =
+    "Curso de Preprocesadores CSS";
 }
 
 // french translation --------------------------
@@ -372,7 +584,7 @@ function french() {
     "Un site web responsive pour vendre des portefeuilles de crypto-monnaie. Vous pouvez ouvrir un compte et commander à partir du panier. Il utilise une base de données SQL et PHP avec MVC. Développé dans le cadre du programme d’études de l’UOC.";
 
   let t240 = document.getElementById("t240");
-  t240.innerHTML = "Un site Web multilingue pour une start-up. Développé avec Laravel 6. Il fait partie du cursus académique de l\'UOC.";
+  t240.innerHTML = "Un site Web multilingue pour une start-up. Développé avec Laravel 6. Il fait partie du cursus académique de l'UOC.";
 
   let t250 = document.getElementById("t250");
   t250.innerHTML = "Cours de Symfony 5 (Master en PHP)";
